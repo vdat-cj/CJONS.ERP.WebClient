646c25d6c5503b179e6cc364d2408b5babf3e59c
vdat-cj <dat.truongvan@cj.net>
2024-12-13 17:31:54 +0700

    refactor: change message validation

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  username: z.string({ required_error: 'Username là bắt buộc!' }).min(1, 'Username là bắt buộc!'),
-  password: z.string({ required_error: 'Mật khẩu là bắt buộc' }).min(1, 'Mật khẩu là bắt buộc!')
+  username: z.string({ required_error: 'Username is required!' }).min(1, 'Username is required!'),
+  password: z.string({ required_error: 'Password is required' }).min(1, 'Password is required!')
 })
